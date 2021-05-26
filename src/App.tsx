93f002fb20fea3158f9f1467a099c27eeb92d7a1
Karthick Ragavendran <karthickthankyou@gmail.com>
2021-05-26 16:31:00 +0530

    fix(dev): configured primary color in tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
 function App() {
-  return <div>Hello World</div>
+  return (
+    <div className="flex">
+      {[50, 100, 200, 300, 400, 500, 600, 700, 800, 900].map((num) => (
+        <div className={`p-4 m-2 bg-primary-${num}`}></div>
+      ))}
+    </div>
+  )
 }
 
 export default App
